68d45581d36dc545c1c2b66b37f4981de8e18847
Abubakar Siddique <abz.siddique@gmail.com>
2020-02-29 13:51:52 +0000

    feat: Add buildNav() function
    This will dynamically create a new list item for every new section added. Each list item links to the section anchor.

--- js/app.js
+++ js/app.js
@@ -7,3 +7,29 @@
 */
 
 //  Ensure the DOM has fully loaded and parsed
+
+// Remember this doesn't wait for CSS!
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    //  Add all section sames to navigation, including future added sections with set data-nav attribute
+    const buildNav = () => {
+
+        const sections = document.querySelectorAll('section[data-nav]');
+        const navbarList = document.getElementById('navbar__list');
+
+        let counter = 1;    // Counter for Section Number
+
+        for (const section of sections) {
+            const sectionName = section.dataset.nav;    // Returns [data-nav] value from Section Element
+            const anchor = `section${counter}`;
+            counter ++;
+
+            navbarList.innerHTML += `<li><a href="#${anchor}">${sectionName}</a></li>`;
+        }
+    
+    }
+
+    buildNav();
+
+});
+
