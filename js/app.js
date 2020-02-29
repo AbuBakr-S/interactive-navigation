19390e5b9c15920f26b439efc01cb17604a248be
Abubakar Siddique <abz.siddique@gmail.com>
2020-02-29 12:48:53 +0000

    feat: Initialise Project File Structure
    Including template code

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*
+
+1. Write a function to loop through all the sections and retrieve the data-nav attribute. 
+2. Create li elements and add the data-nav as text content.
+3. Append these li elements to the navbar__menu class
+
+*/
+
+//  Ensure the DOM has fully loaded and parsed
